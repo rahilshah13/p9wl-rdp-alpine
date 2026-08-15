3403a3a4bfeef393d3b2ea8fe289067a69cab38d
rahilshah13 <rahilshah360@gmail.com>
2026-08-14 21:55:15 -0700

    add debug

--- input/input.c
+++ input/input.c
@@ -22,6 +22,7 @@
 /* ============== Input Queue ============== */
 
 void input_queue_init(struct input_queue *q) {
+    WLog_DBG(TAG, "Entering input_queue_init");
     q->head = q->tail = 0;
     pthread_mutex_init(&q->lock, NULL);
     if (pipe(q->pipe_fd) < 0) {
@@ -29,9 +30,11 @@ void input_queue_init(struct input_queue *q) {
         q->pipe_fd[0] = q->pipe_fd[1] = -1;
     }
     fcntl(q->pipe_fd[0], F_SETFL, O_NONBLOCK);
+    WLog_DBG(TAG, "Exiting input_queue_init");
 }
 
 void input_queue_push(struct input_queue *q, struct input_event *ev) {
+    WLog_DBG(TAG, "Entering input_queue_push");
     pthread_mutex_lock(&q->lock);
     int next = (q->tail + 1) % INPUT_QUEUE_SIZE;
     if (next != q->head) {
@@ -39,25 +42,32 @@ void input_queue_push(struct input_queue *q, struct input_event *ev) {
         q->tail = next;
         char c = 1;
         if (write(q->pipe_fd[1], &c, 1) < 0) { /* ignore */ }
+    } else {
+        WLog_WARN(TAG, "input_queue_push: queue is full");
     }
     pthread_mutex_unlock(&q->lock);
+    WLog_DBG(TAG, "Exiting input_queue_push");
 }
 
 int input_queue_pop(struct input_queue *q, struct input_event *ev) {
+    WLog_DBG(TAG, "Entering input_queue_pop");
     pthread_mutex_lock(&q->lock);
     if (q->head == q->tail) {
         pthread_mutex_unlock(&q->lock);
+        WLog_DBG(TAG, "Exiting input_queue_pop: queue empty");
         return 0;
     }
     *ev = q->events[q->head];
     q->head = (q->head + 1) % INPUT_QUEUE_SIZE;
     pthread_mutex_unlock(&q->lock);
+    WLog_DBG(TAG, "Exiting input_queue_pop: success");
     return 1;
 }
 
 /* ============== FreeRDP Input Callbacks ============== */
 
 void rdp_handle_mouse(struct server *s, uint16_t flags, uint16_t x, uint16_t y) {
+    WLog_DBG(TAG, "Entering rdp_handle_mouse (flags=0x%X, x=%u, y=%u)", flags, x, y);
     int buttons = 0;
     
     if (flags & PTR_FLAGS_BUTTON1) buttons |= 1;
@@ -69,9 +79,11 @@ void rdp_handle_mouse(struct server *s, uint16_t flags, uint16_t x, uint16_t y) 
         .mouse = { .x = x, .y = y, .buttons = buttons }
     };
     input_queue_push(&s->input_queue, &ev);
+    WLog_DBG(TAG, "Exiting rdp_handle_mouse");
 }
 
 void rdp_handle_keyboard(struct server *s, uint16_t flags, uint8_t keycode) {
+    WLog_DBG(TAG, "Entering rdp_handle_keyboard (flags=0x%X, keycode=0x%X)", flags, keycode);
     int pressed = (flags & KBD_FLAGS_DOWN) ? 1 : 0;
 
     struct input_event ev = {
@@ -79,15 +91,20 @@ void rdp_handle_keyboard(struct server *s, uint16_t flags, uint8_t keycode) {
         .key = { .rune = keycode, .pressed = pressed }
     };
     input_queue_push(&s->input_queue, &ev);
+    WLog_DBG(TAG, "Exiting rdp_handle_keyboard");
 }
 
 
 void *mouse_thread_func(void *arg) {
+    WLog_DBG(TAG, "Entering mouse_thread_func");
     (void)arg;
+    WLog_DBG(TAG, "Exiting mouse_thread_func");
     return NULL;
 }
 
 void *kbd_thread_func(void *arg) {
+    WLog_DBG(TAG, "Entering kbd_thread_func");
     (void)arg;
+    WLog_DBG(TAG, "Exiting kbd_thread_func");
     return NULL;
 }
